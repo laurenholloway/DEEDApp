494be4cb507d671aaf2ce2bb5c224d533f15bfa5
Lauren Holloway <laurenholloway1010@gmail.com>
2015-11-02 16:40:50 -0500

    setting up

---
+++ DEEDApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DEEDApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+
+            return View();
+        }
+    }
+}
